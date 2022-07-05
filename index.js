77b59cd8df4b4082819746a44f9d1722269730a5
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-05 19:43:05 +0100

    adding route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 connectDB();
 app.use(express.json({ extented: false }));
 
+// Define Routes
+app.use('/',require('./routes/index'))
+app.use('/api/url',require('./routes/url'))
 const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
---
+++ routes/url.js
@@ -0,0 +1,41 @@
+const express = require
+const router = express.Router();
+const validUrl = require('valid-url');
+const shortid = require('shortid');
+const config = require('config')
+const Url = require('../models/Url');
+const Url = require("../models/Url");
+
+// @routes POST /api/url/shorten
+// @desc create short url
+router.post('/shorten', async (req, res) => {
+    const { longUrl } = req.body;
+    const baseUrl = config.get('baseUrl');
+    if (!validUrl.isUri(baseUrl)) {
+        return res.status(401).json('invalid base url');
+    }
+    // Create url code
+    const urlCode = shortid.generate();
+
+    // Check long url
+    if (validUrl.isUri(longUrl)) {
+        try {
+            let url = await Url.findOne({ longUrl });
+            if (url) {
+                res.json(url);
+            } else {
+                const shortUrl = baseUrl + '/' + urlCode;
+
+                url = new Url({
+                    longUrl,
+                    short
+                })
+            }
+        } catch (err) {
+            
+        }
+    } else {
+        
+    }
+
+});
