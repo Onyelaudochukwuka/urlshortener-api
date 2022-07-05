b6b9598d002316361be779aa25fac88e0369ef21
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-05 20:49:55 +0100

    fixes

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
-const connectDB =  () => {
-  mongoose.connect(db, {
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://udoka:furthermaths$1@cluster0.bmcxh.mongodb.net/?retryWrites=true&w=majority", {
             useNewUrlParser: true,
-            useUnifiedTopology: true
- },
- ).then(() => console.log("Moongoose connected..."))
-     .catch((err) => {
-         console.log(err.message);
-         process.exit(1);
-    })
-     
+        }
+        );
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 }
 module.exports = connectDB;
