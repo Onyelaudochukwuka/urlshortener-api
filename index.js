3b208b34c48aa1569add8a7c31750487f408a286
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-07-05 22:04:02 +0100

    finished server preperation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ connectDB();
 app.use(express.json());
 
 // Define Routes
-// app.use('/',require('./routes/index'))
+app.use('/',require('./routes/index'))
 app.use('/api/url',require('./routes/url'))
 const PORT = 5000;
 
