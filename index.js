2278c38fd34e0958fe394a42dc6c4d8ad528d4ec
Udoka <udochukwukaonyela@gmail.com>
2022-07-11 21:03:27 +0100

    fixes

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 // Define Routes
 app.use('/',require('./routes/index'))
 app.use('/api/url',require('./routes/url'))
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
